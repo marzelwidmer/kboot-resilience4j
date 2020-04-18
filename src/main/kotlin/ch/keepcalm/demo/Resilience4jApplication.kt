a50bcbf3a6260236717feb1e22b7020eefdb01f9
Marcel Widmer <marzelwidmer@gmail.com>
2020-04-18 09:34:36 +0200

    Implement Service

--- src/main/kotlin/ch/keepcalm/demo/Resilience4jApplication.kt
+++ src/main/kotlin/ch/keepcalm/demo/Resilience4jApplication.kt
@@ -1,11 +1,34 @@
 package ch.keepcalm.demo
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import java.time.Duration
 
 @SpringBootApplication
 class Resilience4jApplication
 
 fun main(args: Array<String>) {
-	runApplication<Resilience4jApplication>(*args)
+    runApplication<Resilience4jApplication>(*args)
 }
+
+@Service
+class TurtleService {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    fun readySetGo(name: String?): Mono<TurtleServiceResponse> {
+        name?.map {
+            val delayInSeconds = (0..10).random()
+            val msg = "$name Ready, set, go!! this call took $delayInSeconds"
+            return Mono.just(TurtleServiceResponse(message = msg))
+                .delayElement(Duration.ofSeconds(delayInSeconds.toLong()))
+                .doOnNext { log.info(it.message) }
+        }.isNullOrEmpty()
+            .apply {
+                return Mono.error(NullPointerException())
+            }
+    }
+}
+data class TurtleServiceResponse(val message: String)
