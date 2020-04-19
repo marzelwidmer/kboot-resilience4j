ccb767c1635dc439c3bc8e8445506714485e7fec
Marcel Widmer <marzelwidmer@gmail.com>
2020-04-19 08:02:29 +0200

    Client Implementation

--- src/main/kotlin/ch/keepcalm/demo/Resilience4jApplication.kt
+++ src/main/kotlin/ch/keepcalm/demo/Resilience4jApplication.kt
@@ -2,19 +2,25 @@ package ch.keepcalm.demo
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
 import org.springframework.context.support.beans
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
 import java.time.Duration
 
+
 @SpringBootApplication
 class Resilience4jApplication
 
 fun main(args: Array<String>) {
-    runApplication<Resilience4jApplication>(*args){
+    runApplication<Resilience4jApplication>(*args) {
         addInitializers(
             beans {
                 bean {
@@ -25,10 +31,39 @@ fun main(args: Array<String>) {
                         }
                     }
                 }
+                // Client
+                bean {
+                    WebClient.builder()
+                        .baseUrl("http://localhost:8080")
+                        .build()
+                }
             }
         )
     }
 }
+
+@Component
+class Client(private val webClient: WebClient) {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @EventListener(classes = [ApplicationReadyEvent::class])
+    fun ready() {
+
+        for (count in 1..50) {
+            webClient
+                .get()
+                .uri("/slow/{name}", "[$count]CallFromEventListener")
+                .retrieve()
+                .bodyToMono(TurtleServiceResponse::class.java)
+                .map { it.message }
+                .subscribe {
+                    log.info("--> Client[$count] :  $it")
+                }
+        }
+    }
+}
+
 
 @Service
 class TurtleService {
@@ -40,11 +75,12 @@ class TurtleService {
             val msg = "$name Ready, set, go!! this call took $delayInSeconds"
             return Mono.just(TurtleServiceResponse(message = msg))
                 .delayElement(Duration.ofSeconds(delayInSeconds.toLong()))
-                .doOnNext { log.info(it.message) }
+                .doOnNext { log.info("$it") }
         }.isNullOrEmpty()
             .apply {
-                return Mono.error(NullPointerException())
+                return Mono.empty()
             }
     }
 }
+
 data class TurtleServiceResponse(val message: String)
